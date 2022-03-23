e64963dd74fc59d03cdf801639c5972e1aaff977
Aynur ÇELEBİ <aynurcelebi8947@gmail.com>
2022-03-24 02:58:22 +0300

    spring-boot-first-project
    Spring boot loading and preparation phase

---
+++ src/main/java/com/aynurcelebi/springbootfirstproject/SpringBootFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.aynurcelebi.springbootfirstproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFirstProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aynurcelebi/springbootfirstproject/SpringBootFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aynurcelebi.springbootfirstproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
